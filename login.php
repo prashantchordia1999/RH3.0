3f2376853a1f5373fe49bc5c32e572c8df52e60c
prashantchordia1999 <jprashant28282828@gmail.com>
2017-12-03 01:26:14 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html><head>
+		<title>Mharo Rajasthan | HOME</title>
+		
+		<link rel="stylesheet" href="./files/main.css">
+	</head>
+	<body class="landing" background="#47C0CD" style="font-weight: bold;">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header">
+
+					<h1 id="logo">
+						<a href="#Home" > 
+
+<!--<img src=".\files\download.jpg" width="150" height="150">-->
+						</a></h1>
+				<nav id="nav">
+						<ul>
+				
+							<li style="white-space: nowrap;"style="font-size: 20px;"><a href=".\login.php" target="_blank" class="button special"><strong>Log In</strong></a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner" style="background-position: center -103.35px;background-color: #272833;">
+					<div class="content">
+						<header  style="text-align: center;
+    width: 600px;
+    padding-right: 100px;
+">
+							<h2 style="font-weight: bolder;font-size: 52px;">MHARO - RAJASTHAN</h2>
+							<p><strong>Add Tagline Here</strong><br>
+								<strong>Add a good sub tag here.</strong>
+							</p>
+	                   
+						
+						</header>
+
+
+						<span class="image"><img src="./files/pic011.jpg" alt=""></span>
+				<form method="post" action="" class="container 50%" style="margin-top: 20px;">
+							<div class="row uniform 50%">
+								<div class="10u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Username" style="font-weight: bold;"></div>
+									<div class="5u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Password" style="font-weight: bold;"></div>
+								<div class="5u$ 12u$(xsmall)"><input type="submit" value="Submit" class="fit special"></div>
+							</div>
+						</form>
+					</div>
+				</section>
+	
+			<!-- Footer -->
+				<footer id="footer" style="padding-top: 10px;padding-bottom: 40px;background-color: #ff2000fc;" >
+					<ul class="copyright">
+						
+						<li style="color: white;font-size: 20px;"><strong>© THE REAL LIVING 2017. All rights reserved.</strong></li>
+					</ul>
+				</footer>
+
+		</div>
